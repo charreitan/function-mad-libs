a2d844d85a66a6f1bdb1fbb41abb88cb71bc4fb8
CHARLOTTE <creitan@uvm.edu>
2024-10-21 10:30:30 -0400

    First Commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const typeOfBirdInput = document.getElementById('typeOfBird');
+const typeOfBird = typeOfBirdInput.value;
+
+const RoommateNameInput = document.getElementById('RoommateName');
+const RoommateName = RoommateNameInput.value;
+
+const NounInput = document.getElementById('Noun');
+const Noun = NounInput.value;
+
+const LiquidInput = document.getElementById('Liquid');
+const Liquid = LiquidInput.value;
+
+const BodyPartInput = document.getElementById('BodyPart');
+const BodyPart = BodyPartInput.value;
+
+const generateMadLibs = () => {
+
+}
+
+
+
+
